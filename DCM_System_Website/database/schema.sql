82967590205a00aa1ad294be246a6bbc8ab6b1ae
mallikaaa21 <140768140+mallikaaa21@users.noreply.github.com>
2025-05-01 00:00:12 +0530

    schema.sql

--- DCM_System_Website/database/schema.sql
+++ DCM_System_Website/database/schema.sql
@@ -1,1 +1,7 @@
-CREATE DATABASE IF NOT EXISTS dcm_system;
+
+class Config:
+    SECRET_KEY = 'your_secret_key_here'
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''  # Set your MySQL root password
+    MYSQL_DB = 'dcm_system'
