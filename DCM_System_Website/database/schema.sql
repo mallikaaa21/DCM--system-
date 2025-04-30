819248655ea6e0017e104107b4e192fc98318899
mallikaaa21 <140768140+mallikaaa21@users.noreply.github.com>
2025-05-01 01:39:15 +0530

    Delete DCM_System_Website directory

--- DCM_System_Website/database/schema.sql
+++
@@ -1,7 +0,0 @@
-
-class Config:
-    SECRET_KEY = 'your_secret_key_here'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''  # Set your MySQL root password
-    MYSQL_DB = 'dcm_system'
